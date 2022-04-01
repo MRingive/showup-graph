0cf6f3cec4c2e1ba72f3eb59dd8517fe2e935c65
MRingive <mathiasringive@hotmail.com>
2022-04-01 19:40:21 +0200

    Start work on indexing journey

--- generated/schema.ts
+++ generated/schema.ts
@@ -116,3 +116,66 @@ export class JourneyCreated extends Entity {
     this.set("journeyId", Value.fromBigInt(value));
   }
 }
+
+export class Journey extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("creator", Value.fromBytes(Bytes.empty()));
+    this.set("journeyId", Value.fromBigInt(BigInt.zero()));
+    this.set("description", Value.fromString(""));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save Journey entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        `Entities of type Journey must have an ID of type String but the id '${id.displayData()}' is of type ${id.displayKind()}`
+      );
+      store.set("Journey", id.toString(), this);
+    }
+  }
+
+  static load(id: string): Journey | null {
+    return changetype<Journey | null>(store.get("Journey", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get creator(): Bytes {
+    let value = this.get("creator");
+    return value!.toBytes();
+  }
+
+  set creator(value: Bytes) {
+    this.set("creator", Value.fromBytes(value));
+  }
+
+  get journeyId(): BigInt {
+    let value = this.get("journeyId");
+    return value!.toBigInt();
+  }
+
+  set journeyId(value: BigInt) {
+    this.set("journeyId", Value.fromBigInt(value));
+  }
+
+  get description(): string {
+    let value = this.get("description");
+    return value!.toString();
+  }
+
+  set description(value: string) {
+    this.set("description", Value.fromString(value));
+  }
+}
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,9 @@
 import {
   AttemptCreated as AttemptCreatedEvent,
-  JourneyCreated as JourneyCreatedEvent
+  JourneyCreated as JourneyCreatedEvent,
+  ShowUpClub
 } from "../generated/ShowUpClub/ShowUpClub"
-import { AttemptCreated, JourneyCreated } from "../generated/schema"
+import { AttemptCreated, Journey, JourneyCreated } from "../generated/schema"
 
 export function handleAttemptCreated(event: AttemptCreatedEvent): void {
   let entity = new AttemptCreated(
@@ -20,4 +21,15 @@ export function handleJourneyCreated(event: JourneyCreatedEvent): void {
   entity.creator = event.params.creator
   entity.journeyId = event.params.id
   entity.save()
+
+
+  const journey = ShowUpClub.bind(event.address)
+
+  let journeyEntity = new Journey(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  journeyEntity.creator = event.params.creator
+  journeyEntity.journeyId = event.params.id
+  journeyEntity.description = journey.getJourney(event.params.id).description
+  journeyEntity.save()
 }
